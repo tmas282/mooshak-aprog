31891293a9fc5cee9c96dd76a8f6a0882c7b34c0
Tomás Moreira <tomaspintomoreira28@gmail.com>
2023-11-18 23:09:56 +0000

    Exercicio E: Faça um programa para determinar algumas estatísticas sobre vencimentos de funcionários de uma empresa. O número de funcionários pode variar ao longo do tempo, será pelos menos um, mas nunca será superior a 20. O programa deve mostrar o valor da média dos vencimentos assim como o nome dos funcionários com vencimentos inferior à média.
    Usando modularização, implemente as seguintes funcionalidades:
    
    a) Ler os nomes e vencimentos dos funcionários da empresa. Para cada funcionário deve ser lido o seu nome e respetivo vencimento. A leitura termina  quando for inserido o nome  "end".
    
    b) Calcular a média dos vencimentos.
    
    c) Mostrar resultados. Mostrar em linhas separadas, o valor da média (1 casa decimal) e os nomes dos funcionários com vencimento inferior a essa média.

--- src/week8/ExercicioE.java
+++ src/week8/ExercicioE.java
@@ -1,4 +1,52 @@
 package week8;
 
+import java.util.Scanner;
+
 public class ExercicioE {
+    static final Scanner SCANNER = new Scanner(System.in);
+    static char[][] nomes = new char[20][9];
+    static float[] salarios = new float[20];
+
+    static void lerNomesEVencimentos() {
+        int i=0, k=0;
+        boolean continuar = true;
+        while(continuar){
+            String nome = SCANNER.next();
+            if(!nome.equals("end")){
+                float salario = SCANNER.nextFloat();
+                nomes[i] = nome.toCharArray();
+                i++;
+                salarios[k] = salario;
+                k++;
+            }
+            else {
+                continuar = false;
+            }
+        }
+
+    }
+    static float calcularMedia(float[] valores) {
+        float nume=0, deno= (float) valores.length;
+        for (int i = 0; i < valores.length; i++) {
+            nume+=valores[i];
+            if(valores[i] == 0){
+                deno--;
+            }
+        }
+        return nume / deno;
+    }
+    static void mostrarValores(float media, char[][] nomes, float[] valores) {
+        System.out.printf("%.1f%n", media);
+        for (int i = 0; i < valores.length; i++) {
+            if(valores[i] != 0 && valores[i] < media){
+                String nome = String.copyValueOf(nomes[i]);
+                System.out.println(nome);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        lerNomesEVencimentos();
+        float media = calcularMedia(salarios);
+        mostrarValores(media, nomes, salarios);
+    }
 }
